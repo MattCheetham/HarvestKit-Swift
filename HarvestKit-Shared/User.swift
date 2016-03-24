873f04f0da45e8aa438c2b536b6d1befcb5ba81c
Matt Cheetham <m4ttcheetham@gmail.com>
2016-03-24 17:44:44 +0000

    Adds missing properties from user object when using who am I method #14

--- HarvestKit-Shared/User.swift
+++ HarvestKit-Shared/User.swift
@@ -39,9 +39,69 @@ public struct User {
     public var active: Bool?
     
     /**
+    A boolean to indicate whether or not the user is an admin in this system.
+    - note: Only populated when created from the Who Am I call
+    */
+    public var admin: Bool?
+    
+    /**
     The name of the department that the user belongs to
     */
     public var department: String?
+    
+    /**
+    The timezone identifier as a string that the user has their account set to. E.g. "Europe/London"
+     - note: Only populated when created from the Who Am I call
+    */
+    public var timezoneIdentifier: String?
+    
+    /**
+    The timezone city. The harvest API doesn't give any documentation on this so I'm going to assume it always returns the city part of the `timezoneIdentifier`
+     - note: Only populated when created from the Who Am I call
+    */
+    public var timezoneCity: String?
+    
+    /**
+    A boolean indicating whether or not the user is a project manager
+    - note: Only populated when created from the Who Am I call
+     */
+    public var projectManager: Bool?
+    
+    /**
+     A boolean indicating whether or not the user can create new projects
+     - note: Only populated when created from the Who Am I call
+     */
+    public var createProjects: Bool?
+    
+    /**
+     A boolean indicating whether or not the user can see hourly rates
+     - note: Only populated when created from the Who Am I call
+     */
+    public var seeRates: Bool?
+    
+    /**
+     A boolean indicating whether or not the user can create new invoices
+     - note: Only populated when created from the Who Am I call
+     */
+    public var createInvoices: Bool?
+    
+    /**
+    The number of seconds that the user is offset from UTC with their current timezone settings
+     - note: Only populated when created from the Who Am I call
+    */
+    public var timezoneOffsetSeconds: Int?
+    
+    /**
+    No information was found about this property in the Harvest API Documentation
+     - note: Only populated when created from the Who Am I call
+    */
+    public var timestampTimers: Bool?
+    
+    /**
+    The URL to the users avatar if they have one
+     - note: Only populated when created from the Who Am I call
+    */
+    public var avatarURL: NSURL?
     
     internal init?(dictionary: [String: AnyObject]) {
         
@@ -55,9 +115,26 @@ public struct User {
         lastName = userDictionary["last_name"] as? String
         email = userDictionary["email"] as? String
         active = userDictionary["is_active"] as? Bool
+        admin = userDictionary["admin"] as? Bool
         department = userDictionary["department"] as? String
-
-
+        timezoneIdentifier = userDictionary["timezone_identifier"] as? String
+        timezoneCity = userDictionary["timezone"] as? String
+        
+        if let projectManagerDictionary = userDictionary["project_manager"] as? [String: AnyObject] {
+            
+            projectManager = projectManagerDictionary["is_project_manager"] as? Bool
+            createProjects = projectManagerDictionary["can_create_projects"] as? Bool
+            seeRates = projectManagerDictionary["can_see_rates"] as? Bool
+            createInvoices = projectManagerDictionary["can_create_invoices"] as? Bool
+        }
+        
+        timezoneOffsetSeconds = userDictionary["timezone_utc_offset"] as? Int
+        timestampTimers = userDictionary["timestamp_timers"] as? Bool
+        
+        if let avatarURLString = userDictionary["avatar_url"] as? String {
+            avatarURL = NSURL(string: avatarURLString)
+        }
+        
     }
     
 }
