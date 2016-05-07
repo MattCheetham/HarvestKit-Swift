44d189440f2edf05e73a1415fa30453bac4231c9
Matt Cheetham <m4ttcheetham@gmail.com>
2016-05-07 10:16:34 +0100

    Adds missing properties to client object

--- HarvestKit-Shared/Client.swift
+++ HarvestKit-Shared/Client.swift
@@ -9,8 +9,8 @@
 import Foundation
 
 /**
-A struct representation of a client in the harvest system
-*/
+ A struct representation of a client in the harvest system
+ */
 public struct Client {
     
     /**
@@ -23,13 +23,15 @@ public struct Client {
      */
     public var name: String?
     
-    /**
-     Any additional details about the client
-     */
     public var details: String?
     
     /**
-     The currency that the client makes/recieves payments in
+     The currency that the client makes/recieves payments in. As a full length description.
+     */
+    public var currencyName: String?
+    
+    /**
+     The currency symbol representing the currency that the client makes/recieves payments in
      */
     public var currencySymbol: String?
     
@@ -37,6 +39,33 @@ public struct Client {
      A boolean to indicate whether or not the client is active in the system. If false, this client has been archived
      */
     public var active: Bool?
+    
+    public var defaultInvoiceKind: String?
+    
+    public var lastInvoiceKind: String?
+    
+    public var defaultInvoiceTimeframe: String?
+    
+    /**
+     The date that the client was created
+     */
+    public var created: NSDate?
+    
+    /**
+     The date that this client was last updated
+     */
+    public var updated: NSDate?
+    
+    public var highriseId: Int?
+    
+    /**
+     The clients address that they can be contacted at
+     */
+    public var address: String?
+    
+    public var cacheVersion: Int?
+    
+    public var statementKey: String?
     
     internal init?(dictionary: [String: AnyObject]) {
         
@@ -48,9 +77,26 @@ public struct Client {
         identifier = clientDictionary["id"] as? Int
         name = clientDictionary["name"] as? String
         details = clientDictionary["details"] as? String
+        currencyName = clientDictionary["currency"] as? String
         currencySymbol = clientDictionary["currency_symbol"] as? String
         active = clientDictionary["active"] as? Bool
+        lastInvoiceKind = clientDictionary["last_invoice_kind"] as? String
+        cacheVersion = clientDictionary["cache_version"] as? Int
         
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+        
+        if let createdDateString = clientDictionary["created_at"] as? String {
+            created = dateFormatter.dateFromString(createdDateString)
+        }
+        
+        if let updatedDateString = clientDictionary["updated_at"] as? String {
+            updated = dateFormatter.dateFromString(updatedDateString)
+        }
+        
+        highriseId = clientDictionary["highrise_id"] as? Int
+        defaultInvoiceTimeframe = clientDictionary["default_invoice_timeframe"] as? String
+        statementKey = clientDictionary["statement_key"] as? String
     }
     
 }
