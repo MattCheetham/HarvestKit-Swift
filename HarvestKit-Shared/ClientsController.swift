1bde3d66b843b0e30eb0e9f18e633f16689b774a
Matt Cheetham <m4ttcheetham@gmail.com>
2016-05-07 10:38:00 +0100

    Adds create method to client controller. Closes #22

--- HarvestKit-Shared/ClientsController.swift
+++ HarvestKit-Shared/ClientsController.swift
@@ -34,6 +34,41 @@ public final class ClientsController {
         self.requestController = requestController
     }
     
+    //MARK - Creating Clients
+    
+    /**
+     Creates a new client entry in the Harvest system. You may configure any of the parameters on the contact object you are creating and they will be saved.
+     
+     - parameter client: The new client object to send to the API
+     - parameter completionHandler: The completion handler to return any errors to
+     
+     - requires: `name` on the client object as a minimum
+     */
+    public func create(client: Client, completion: (error: ErrorType?) -> ()) {
+        
+        guard let _ = client.name else {
+            completion(error: ClientError.MissingName)
+            return
+        }
+        
+        requestController.post("clients", bodyParams: client.serialisedObject) { (response: TSCRequestResponse?, error: NSError?) in
+            
+            if let _error = error {
+                completion(error: _error)
+                return
+            }
+            
+            if let responseStatus = response?.status where responseStatus == 201 {
+                completion(error: nil)
+                return
+            }
+            
+            completion(error: ClientError.UnexpectedResponseCode)
+        }
+    }
+    
+    //MARK - Requesting Clients
+    
     /**
      Requests a specific client from the API by identifier
      
@@ -64,4 +99,8 @@ public final class ClientsController {
 enum ClientError: ErrorType {
     /** The data we got back from the server was not suitable to be converted into a client object */
     case MalformedData
+    /** The client object given did not have a name set so could not be saved */
+    case MissingName
+    /** We got an unexpected response */
+    case UnexpectedResponseCode
 }
